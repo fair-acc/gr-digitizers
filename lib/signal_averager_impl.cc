91d240306a4f046f50becff43a1bcac75c985665
aschwinn <al.schwinn@gsi.de>
2019-02-06 13:54:45 +0100

    Forward tags accoring to gnuradio tag propagation policy

--- lib/signal_averager_impl.cc
+++ lib/signal_averager_impl.cc
@@ -26,10 +26,15 @@ namespace gr {
     signal_averager_impl::signal_averager_impl(int num_inputs, int window_size)
       : gr::sync_decimator("signal_averager",
               gr::io_signature::make(num_inputs, num_inputs, sizeof(float)),
-              gr::io_signature::make(num_inputs, num_inputs, sizeof(float)), window_size),
+              gr::io_signature::make(num_inputs, num_inputs, sizeof(float)),
+              window_size),
         d_num_ports(num_inputs)
     {
-      set_tag_propagation_policy(tag_propagation_policy_t::TPP_DONT);
+
+      // The gr scheduler uses the block's gr::block::relative_rate concept to perform the update on the tag's offset value
+      // The relative rate of a block determines the relationship between the input rate and output rate.
+      // Decimators that decimate by a factor of D have a relative rate of 1/D.
+      set_tag_propagation_policy(tag_propagation_policy_t::TPP_ONE_TO_ONE ); //
     }
 
     signal_averager_impl::~signal_averager_impl()
@@ -42,78 +47,20 @@ namespace gr {
         gr_vector_void_star &output_items)
     {
       unsigned decim = decimation();
-
       for(int port = 0; port < d_num_ports; port++)
       {
-
-//        auto count0 = nitems_read(port);
-
-        //average signal
         float *out = (float *) output_items[port];
         const float *in = (const float *) input_items[port];
-
         for(int i = 0; i < noutput_items; i++)
         {
-
-          // Signal averaging part
           float sum = 0.0f;
-
           for(unsigned j = 0; j < decim; j++)
-          {
             sum += in[j];
-          }
 
           out[i] = sum / static_cast<float>(decim);
           in += decim;
-
-//          // Tag correction/propagation part
-//          std::vector<gr::tag_t> tags;
-//          auto local_count0 = count0 + static_cast<uint64_t>(i * decim);
-//          get_tags_in_range(tags, port, local_count0, local_count0 + decim);
-//
-//          // Merge acq info tags if more than one falls into the same bucket
-//          boost::optional<acq_info_t> merged_acq_info_tag = boost::none;
-//
-//          for(auto tag : tags) {
-//            tag.offset *= (1.0 / decim);
-//
-//            auto tag_key = pmt::symbol_to_string(tag.key);
-//
-//            if(tag_key == timebase_info_tag_name) {
-//              auto timebase = decode_timebase_info_tag(tag);
-//
-//              // Fix timebase
-//              auto tmp =  make_timebase_info_tag(timebase * static_cast<double>(decim));
-//              tmp.offset = tag.offset;
-//              add_item_tag(port, tmp);
-//            }
-//            else if (tag_key == acq_info_tag_name) {
-//              auto acq_info = decode_acq_info_tag(tag);
-//
-//              if (merged_acq_info_tag) {
-//                merged_acq_info_tag->status |= acq_info.status;
-//              }
-//              else {
-//                acq_info.pre_samples /= decim;
-//                acq_info.samples /= decim;
-//                acq_info.timebase *= static_cast<double>(decim);
-//
-//                merged_acq_info_tag = acq_info;
-//              }
-//            }
-//            else {
-//              add_item_tag(port, tag);
-//            }
-//          }
-//
-//          if (merged_acq_info_tag) {
-//            auto tag = make_acq_info_tag(*merged_acq_info_tag);
-//            add_item_tag(port, tag);
-//          }
-//
-          }
+        }
       }
-
       return noutput_items;
     }
 
