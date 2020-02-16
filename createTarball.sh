bfd5f83d635bdfaa55a7db09624f982884892ee5
Alexander Schwinn <al.schwinn@gsi.de>
2020-02-16 23:27:16 +0100

    creation of majpr minor symlinks for tarball

--- createTarball.sh
+++ createTarball.sh
@@ -13,6 +13,8 @@ if [ $# -ne 1 ]
 fi
 
 VERSION=$1
+MAJOR=`echo $VERSION | cut -d. -f1`
+MINOR=`echo $VERSION | cut -d. -f2`
 FOLDER_TO_TAR=usr
 INSTALL_DIR_LIB=${FOLDER_TO_TAR}/lib64
 INSTALL_DIR_BIN=${FOLDER_TO_TAR}/bin
@@ -21,6 +23,9 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")     # path where this script is located in
 
 TARBALL_NAME=Digitizer-${VERSION}.tar
+TARBALL_NAME_MAJOR=Digitizer-${MAJOR}.tar
+TARBALL_NAME_MAJOR_MINOR=Digitizer-${MAJOR}.${MINOR}.tar
+INSTALL_PATH_ASL=/common/export/fesa/arch/x86_64
 
 mkdir -p ${INSTALL_DIR_LIB}
 mkdir -p ${INSTALL_DIR_BIN}
@@ -35,5 +40,16 @@ tar cfv ${TARBALL_NAME} ${FOLDER_TO_TAR}
 rm -rf ${FOLDER_TO_TAR}
 gzip ${TARBALL_NAME}
 
-cp ${TARBALL_NAME}.gz /common/export/fesa/arch/x86_64
-echo "${TARBALL_NAME}.gz copied to /common/export/fesa/arch/x86_64"
+cp ${TARBALL_NAME}.gz ${INSTALL_PATH_ASL}
+echo "${TARBALL_NAME}.gz copied to ${INSTALL_PATH_ASL}"
+
+if [ -L ${INSTALL_PATH_ASL}/${TARBALL_NAME_MAJOR}.gz ]; then
+  unlink ${INSTALL_PATH_ASL}/${TARBALL_NAME_MAJOR}.gz
+fi
+if [ -L ${INSTALL_PATH_ASL}/${TARBALL_NAME_MAJOR_MINOR}.gz ]; then
+  unlink ${INSTALL_PATH_ASL}/${TARBALL_NAME_MAJOR_MINOR}.gz
+fi
+ln -s ${TARBALL_NAME_MAJOR_MINOR}.gz ${INSTALL_PATH_ASL}/${TARBALL_NAME_MAJOR}.gz
+ln -s ${TARBALL_NAME}.gz ${INSTALL_PATH_ASL}/${TARBALL_NAME_MAJOR_MINOR}.gz
+echo "symlinks updated"
+
