ed513e3bffcd7db38c24621587decf34f5339a12
C-N-E <christoph.neumann@infoteam.de>
2021-11-19 06:37:22 -0800

    switched to ps plus power; fixed ps plus power test

--- examples/cpp/ps4000a_plus_power_calc.cc
+++ examples/cpp/ps4000a_plus_power_calc.cc
@@ -38,8 +38,8 @@ void wire_streaming(int time)
 
     auto power_calc_block = power_calc::make(0.00001);
 
-    auto zeromq_pub_sink = gr::zeromq::pub_sink::make(sizeof(float), 8, const_cast<char *>("tcp://*:5001"), 100, false, -1);
-    auto blocks_streams_to_vector = gr::blocks::streams_to_vector::make(sizeof(float)*1, 8);
+    auto zeromq_pub_sink = gr::zeromq::pub_sink::make(sizeof(float), 4, const_cast<char *>("tcp://10.0.0.2:5001"), 100, false, -1);
+    auto blocks_streams_to_vector = gr::blocks::streams_to_vector::make(sizeof(float)*1, 4);
 
     auto band_pass_filter_0_0 = gr::filter::fir_filter_fcc::make(
         100.0,
@@ -97,10 +97,6 @@ void wire_streaming(int time)
     top->connect(power_calc_block, 1, blocks_streams_to_vector, 1);
     top->connect(power_calc_block, 2, blocks_streams_to_vector, 2);
     top->connect(power_calc_block, 3, blocks_streams_to_vector, 3);
-    top->connect(power_calc_block, 4, blocks_streams_to_vector, 4);
-    top->connect(power_calc_block, 5, blocks_streams_to_vector, 5);
-    top->connect(power_calc_block, 6, blocks_streams_to_vector, 6);
-    top->connect(power_calc_block, 7, blocks_streams_to_vector, 7);
 
     top->connect(ps, 0, band_pass_filter_0_0, 0);
     top->connect(ps, 2, band_pass_filter_0, 0);
