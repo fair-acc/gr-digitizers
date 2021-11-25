7977d098d5ada6158a603f2b203761eb37b337bd
C-N-E <christoph.neumann@infoteam.de>
2021-11-25 03:43:19 -0800

    added resampler and raw out to power test; removed rms out

--- lib/power_calc_impl.cc
+++ lib/power_calc_impl.cc
@@ -28,7 +28,7 @@ namespace gr {
     power_calc_impl::power_calc_impl(double alpha)
       : gr::sync_block("power_calc",
               gr::io_signature::make(2 /* min inputs */, 2 /* max inputs */, sizeof(gr_complex)),
-              gr::io_signature::make(6 /* min outputs */, 6 /*max outputs */, sizeof(float)))
+              gr::io_signature::make(4 /* min outputs */, 4 /*max outputs */, sizeof(float)))
     {
       set_alpha(alpha);
     }
@@ -151,8 +151,8 @@ namespace gr {
       float* phi_out = (float*)output_items[3];
       // float* timestamp_ms = (float*)output_items[4];
       
-      float* rms_u = (float*)output_items[4];//(float*)malloc(noutput_items*sizeof(float));
-      float* rms_i = (float*)output_items[5];//(float*)malloc(noutput_items*sizeof(float));
+      float* rms_u = (float*)malloc(noutput_items*sizeof(float));
+      float* rms_i = (float*)malloc(noutput_items*sizeof(float));
 
       calc_rms_u(rms_u, u_in, noutput_items);
       calc_rms_i(rms_i, i_in, noutput_items);
@@ -163,8 +163,8 @@ namespace gr {
       calc_reactive_power(q_out, rms_u, rms_i, phi_out, noutput_items);
       calc_apparent_power(s_out, rms_u, rms_i, noutput_items);
 
-      // free(rms_u);
-      // free(rms_i);
+      free(rms_u);
+      free(rms_i);
 
       // get_timestamp_ms(timestamp_ms);
 
