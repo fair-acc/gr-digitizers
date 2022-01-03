99b87c1bf3cea32a092be5d4681c4dc95423d2d1
Jeremias Steinbauer <jeremias.steinbauer@infoteam.de>
2022-01-03 07:30:31 -0800

    adjusted cmake to properly import picoscope

--- cmake/Modules/Findpicoscope.cmake
+++ cmake/Modules/Findpicoscope.cmake
@@ -2,10 +2,10 @@
 
 FIND_PATH(LIBPICO_INCLUDE_DIR NAMES ps4000aApi.h
                 HINTS /usr/include /opt/picoscope/include/libps4000a/)
-message("LIBPICO_INCLUDE_DIR: " ${LIBPICO_INCLUDE_DIR})
+#message("LIBPICO_INCLUDE_DIR: " ${LIBPICO_INCLUDE_DIR})
 FIND_LIBRARY(LIBPICO_LIBRARY NAMES ps4000a libps4000a picoscope-ps4000a
                 HINTS /usr/lib64 /opt/picoscope/lib)
-message("LIBPICO_LIBRARY: " ${LIBPICO_LIBRARY})
+#message("LIBPICO_LIBRARY: " ${LIBPICO_LIBRARY})
 
 #MARK_AS_ADVANCED(LIBPICO_INCLUDE_DIR LIBPICO_LIBRARY)
 
