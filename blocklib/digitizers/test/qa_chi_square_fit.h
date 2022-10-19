1d0ae2a9117c656ca18ec03a6a0a590596d1049f
Frank Osterfeld <frank.osterfeld@kdab.com>
2022-10-19 19:38:22 +0200

    Port chi_square_fit
    Adds the ROOT lib dependency.

---
+++ blocklib/digitizers/test/qa_chi_square_fit.h
@@ -0,0 +1,21 @@
+#ifndef _QA_CHI_SQUARE_FIT_H_
+#define _QA_CHI_SQUARE_FIT_H_
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestCase.h>
+
+namespace gr::digitizers {
+
+class qa_chi_square_fit : public CppUnit::TestCase {
+public:
+    CPPUNIT_TEST_SUITE(qa_chi_square_fit);
+    CPPUNIT_TEST(test_chi_square_simple_fitting);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    void test_chi_square_simple_fitting();
+};
+
+} /* namespace gr::digitizers */
+
+#endif /* _QA_CHI_SQUARE_FIT_H_ */
