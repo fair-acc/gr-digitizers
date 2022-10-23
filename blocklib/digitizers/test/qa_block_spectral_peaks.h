73e97a7cd619faa81e10d3b2ce912ede60c41cf2
Frank Osterfeld <frank.osterfeld@kdab.com>
2022-10-23 15:56:28 +0200

    Port block_spectral_peaks

---
+++ blocklib/digitizers/test/qa_block_spectral_peaks.h
@@ -0,0 +1,21 @@
+#ifndef _QA_BLOCK_SPECTRAL_PEAKS_H_
+#define _QA_BLOCK_SPECTRAL_PEAKS_H_
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestCase.h>
+
+namespace gr::digitizers {
+
+class qa_block_spectral_peaks : public CppUnit::TestCase {
+public:
+    CPPUNIT_TEST_SUITE(qa_block_spectral_peaks);
+    CPPUNIT_TEST(test_spectral_peaks);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    void test_spectral_peaks();
+};
+
+} /* namespace gr::digitizers */
+
+#endif /* _QA_BLOCK_SPECTRAL_PEAKS_H_ */
