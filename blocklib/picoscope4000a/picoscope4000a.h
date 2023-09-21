c4f835a6065b3894f331d89d5b22f8a4a85aa37e
Frank Osterfeld <frank.osterfeld@kdab.com>
2023-09-21 22:44:47 +0200

    Add all 16 ports

--- blocklib/picoscope4000a/picoscope4000a.h
+++ blocklib/picoscope4000a/picoscope4000a.h
@@ -8,13 +8,41 @@ namespace gr::picoscope4000a {
 struct Picoscope4000a : public gr::picoscope::Picoscope<Picoscope4000a> {
     fair::graph::OUT<float> values0;
     fair::graph::OUT<float> errors0;
+    fair::graph::OUT<float> values1;
+    fair::graph::OUT<float> errors1;
+    fair::graph::OUT<float> values2;
+    fair::graph::OUT<float> errors2;
+    fair::graph::OUT<float> values3;
+    fair::graph::OUT<float> errors3;
+    fair::graph::OUT<float> values4;
+    fair::graph::OUT<float> errors4;
+    fair::graph::OUT<float> values5;
+    fair::graph::OUT<float> errors5;
+    fair::graph::OUT<float> values6;
+    fair::graph::OUT<float> errors6;
+    fair::graph::OUT<float> values7;
+    fair::graph::OUT<float> errors7;
 
     explicit Picoscope4000a(gr::picoscope::Settings settings = {}) : gr::picoscope::Picoscope<Picoscope4000a>(std::move(settings)) {}
 
-    fair::graph::work_return_status_t process_bulk(std::span<float> values,
-                                                   std::span<float> errors) noexcept
+    fair::graph::work_return_status_t process_bulk(std::span<float> v0,
+                                                   std::span<float> e0,
+                                                   std::span<float> v1,
+                                                   std::span<float> e1,
+                                                   std::span<float> v2,
+                                                   std::span<float> e2,
+                                                   std::span<float> v3,
+                                                   std::span<float> e3,
+                                                   std::span<float> v4,
+                                                   std::span<float> e4,
+                                                   std::span<float> v5,
+                                                   std::span<float> e5,
+                                                   std::span<float> v6,
+                                                   std::span<float> e6,
+                                                   std::span<float> v7,
+                                                   std::span<float> e7) noexcept
     {
-        return this->process_bulk_impl<1>({{values, errors}});
+        return this->process_bulk_impl<8>({{{v0, e0}, {v1, e1}, {v2, e2}, {v3, e3}, {v4, e4}, {v5, e5}, {v6, e6}, {v7, e7}}});
     }
 
     std::make_signed_t<std::size_t>
@@ -39,6 +67,6 @@ struct Picoscope4000a : public gr::picoscope::Picoscope<Picoscope4000a> {
 
 } // namespace gr::picoscope4000a
 
-ENABLE_REFLECTION(gr::picoscope4000a::Picoscope4000a, values0, errors0);
+ENABLE_REFLECTION(gr::picoscope4000a::Picoscope4000a, values0, errors0, values1, errors1, values2, errors2, values3, errors3, values4, errors4, values5, errors5, values6, errors6, values7, errors7);
 
 #endif
