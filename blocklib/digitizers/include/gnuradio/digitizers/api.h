640403070efac9fb2c3fe8134e38338c40f1ba3e
Frank Osterfeld <frank.osterfeld@kdab.com>
2023-09-28 16:40:03 +0200

    Delete unported code

--- blocklib/digitizers/include/gnuradio/digitizers/api.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <gnuradio/attributes.h>
-
-#ifdef gnuradio_digitizers_EXPORTS
-#define DIGITIZERS_API __GR_ATTR_EXPORT
-#else
-#define DIGITIZERS_API __GR_ATTR_IMPORT
-#endif
--- blocklib/picoscope3000a/python/gnuradio/picoscope3000a/__init__.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-try:
-    from .picoscope3000a_python import *
-except ImportError:
-    dirname, filename = os.path.split(os.path.abspath(__file__))
-    __path__.append(os.path.join(dirname, "bindings"))
-    from .picoscope3000a_python import *
